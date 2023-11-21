469246711ffdb339ff6c0f96f58e9228a102509b
= <kaweesha.mr@gmail.com>
2023-11-21 10:29:15 +0530

    tute 10

--- dojo-blog/src/home.js
+++ dojo-blog/src/home.js
@@ -4,25 +4,42 @@ const Home = () => {
 
   
 
-    //this variable array stores 2 values which will use to pass the value to be change and other to identify it
-    const [name,setName] = useState('mario')
-    const [age,setAge] = useState(25);
+    // //this variable array stores 2 values which will use to pass the value to be change and other to identify it
+    // const [name,setName] = useState('mario')
+    // const [age,setAge] = useState(25);
    
-    const handleCLick = () =>{
+    // const handleCLick = () =>{
         
-        //this function will change the value of name variable
-        setName('luigi');
-        setAge(30);
+    //     //this function will change the value of name variable
+    //     setName('luigi');
+    //     setAge(30);
 
     
-    }
+    // }
 
+
+    const [blog,setBlog] = useState([
+
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+
+    ])
     return (  
 
         <div className="home">
-            <h2>Home Page</h2>
-            <p>{name} is {age} years old</p>
-            <button onClick={handleCLick}>Click me</button>
+
+
+            {blog.map((blog) => (
+
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>written by {blog.author}</p>
+
+                </div>
+            ))}
+
+
         </div>
 
     );
