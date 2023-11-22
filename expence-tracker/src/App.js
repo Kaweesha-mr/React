f6b2f6061ba9e102304186b426f1b55c83bb8bf7
= <kaweesha.mr@gmail.com>
2023-11-22 22:50:16 +0530

    Delete unnecessary files and code

---
+++ expence-tracker/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Header from './Header';
+import Blance from './components/Blance';
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <div className="container">
+      <Blance/>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
