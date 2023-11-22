b1a020be570b50cdeaeaacf54fab2b7be369120f
= <kaweesha.mr@gmail.com>
2023-11-22 18:39:38 +0530

    Refactor routing in App.js and add link to BlogDetails in BlogList.js

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,26 +1,29 @@
 import Navbar from './navbar';
 import Home from './home';
-import { BrowserRouter as Router,Route, Switch} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Create from './create';
+import BlogDetails from './BlogDetails';
 
 function App() {
   return (
     <Router>    <div className="App">
       <Navbar />
-      
-      <div className="content">
-          
-          <Switch>
-            <Route exact path="/create">
-              <Create />
-            </Route>
-            <Route exact path="/">
-              <Home />
-            </Route>
 
-          </Switch>
+      <div className="content">
+        <Switch>
+          <Route exact path="/create">
+            <Create />
+          </Route>
+          <Route path="/blogs/:id">
+            <BlogDetails />
+          </Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
+
+        </Switch>
       </div>
-    
+
     </div>
     </Router>
   );
--- dojo-blog/src/BlogList.js
+++ dojo-blog/src/BlogList.js
@@ -1,14 +1,20 @@
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+
+
 const BlogList = ({blogs,title}) => {
 
     return (  
-
         <div className="blog-list">
 
             <h2>{title}</h2>
             {blogs.map((blogs) => (
+
             <div className="blog-preview" key={blogs.id}>
+                <Link to ={`/blogs/${blogs.id}`}>
                 <h2>{blogs.title}</h2>
                 <p>Written by {blogs.author}</p>
+                </Link>
+
             </div>
             ))}
         </div>
