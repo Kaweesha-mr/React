14ea8450e36a1b5424c5e0d5122dbc29c96a1e71
= <kaweesha.mr@gmail.com>
2023-12-02 14:15:20 +0530

    .

---
+++ contextapp/src/App.js
@@ -0,0 +1,16 @@
+import BookList from "./components/BookList";
+import Navbar from "./components/navbar";
+
+function App() {
+  return (
+    <div className="App">
+
+
+      <Navbar />
+      <BookList/>
+
+    </div>
+  );
+}
+
+export default App;
