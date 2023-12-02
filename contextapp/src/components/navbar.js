5227a2a1bc73558c9fdaa40109eff3158089a219
= <kaweesha.mr@gmail.com>
2023-12-02 23:27:24 +0530

    another method for accessing class components

--- contextapp/src/components/navbar.js
+++ contextapp/src/components/navbar.js
@@ -4,28 +4,35 @@ import { ThemeContext } from "../context/ThemeContext";
 
 class Navbar extends Component {
 
-    //accessing the context in components
-    static contextType = ThemeContext;
+
 
     state = {  } 
     render() { 
-        console.log(this.context);
+
 
         //destructuring the context and store the values in variables
         const {isLightTheme, light, dark} = this.context;
 
         const theme = isLightTheme ? light : dark;
         return (
-            <nav style={ { background:theme.ui, color: theme.syntax}}>
-                <h1>Context App</h1>
-                <ul>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Contact Us</li>
-                </ul>
-            </nav>
+            <ThemeContext.Consumer>{(context)=>{
+                const {isLightTheme, light, dark} = context;
+                const theme = isLightTheme ? light : dark;
+                return(
+                    <nav style={{background: theme.ui, color: theme.syntax}}>
+                        <h1>Context App</h1>
+                        <ul>
+                            <li>Home</li>
+                            <li>About</li>
+                            <li>Contact</li>
+                        </ul>
+
+                    </nav>
+                )
+            }}</ThemeContext.Consumer>
         );
     }
 }
+
  
 export default Navbar;
